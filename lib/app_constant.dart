12b209fe719000b807578b70efc4837744ce15b4
Rizky Eka Haryadi <eka28rizky03haryadi04@gmail.com>
2023-01-11 11:29:26 +0700

    First Commit

---
+++ lib/app_constant.dart
@@ -0,0 +1,4 @@
+class AppConstant {
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String apiKey = '41985d94d0f419cd140a835bd69994cb';
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyApp());
+  FlutterNativeSplash.remove();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
