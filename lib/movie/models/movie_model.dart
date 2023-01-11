dbe5c2a844a471ad8a702c2d0d2627c077e31339
Rizky Eka Haryadi <eka28rizky03haryadi04@gmail.com>
2023-01-11 20:12:55 +0700

    First Commit

--- lib/movie/models/movie_model.dart
+++ lib/movie/models/movie_model.dart
@@ -1,10 +1,3 @@
-// To parse this JSON data, do
-//
-//     final movieResponseModel = movieResponseModelFromJson(jsonString);
-
-import 'package:meta/meta.dart';
-import 'dart:convert';
-
 class MovieResponseModel {
   MovieResponseModel({
     required this.page,
@@ -13,31 +6,16 @@ class MovieResponseModel {
     required this.totalResults,
   });
 
-  final int? page;
-  final List<MovieModel?>? results;
-  final int? totalPages;
-  final int? totalResults;
-
-  MovieResponseModel copyWith({
-    int? page,
-    List<MovieModel?>? results,
-    int? totalPages,
-    int? totalResults,
-  }) =>
-      MovieResponseModel(
-        page: page ?? this.page,
-        results: results ?? this.results,
-        totalPages: totalPages ?? this.totalPages,
-        totalResults: totalResults ?? this.totalResults,
-      );
+  final int page;
+  final List<MovieModel> results;
+  final int totalPages;
+  final int totalResults;
 
   factory MovieResponseModel.fromMap(Map<String, dynamic> json) =>
       MovieResponseModel(
         page: json["page"],
-        results: json["results"] == null
-            ? []
-            : List<MovieModel?>.from(
-                json["results"]!.map((x) => MovieModel.fromJson(x))),
+        results: List<MovieModel>.from(
+            json["results"].map((x) => MovieModel.fromMap(x))),
         totalPages: json["total_pages"],
         totalResults: json["total_results"],
       );
@@ -46,73 +24,27 @@ class MovieResponseModel {
 class MovieModel {
   MovieModel({
     this.backdropPath,
-    required this.genreIds,
     required this.id,
-    required this.originalTitle,
     required this.overview,
-    required this.popularity,
     this.posterPath,
-    required this.releaseDate,
     required this.title,
     required this.voteAverage,
     required this.voteCount,
   });
 
   final String? backdropPath;
-  final List<int?>? genreIds;
-  final int? id;
-
-  final String? originalTitle;
-  final String? overview;
-  final double? popularity;
+  final int id;
+  final String overview;
   final String? posterPath;
-  final DateTime? releaseDate;
-  final String? title;
+  final String title;
+  final double voteAverage;
+  final int voteCount;
 
-  final double? voteAverage;
-  final int? voteCount;
-
-  MovieModel copyWith({
-    bool? adult,
-    String? backdropPath,
-    List<int?>? genreIds,
-    int? id,
-    String? originalLanguage,
-    String? originalTitle,
-    String? overview,
-    double? popularity,
-    String? posterPath,
-    DateTime? releaseDate,
-    String? title,
-    bool? video,
-    double? voteAverage,
-    int? voteCount,
-  }) =>
-      MovieModel(
-        backdropPath: backdropPath ?? this.backdropPath,
-        genreIds: genreIds ?? this.genreIds,
-        id: id ?? this.id,
-        originalTitle: originalTitle ?? this.originalTitle,
-        overview: overview ?? this.overview,
-        popularity: popularity ?? this.popularity,
-        posterPath: posterPath ?? this.posterPath,
-        releaseDate: releaseDate ?? this.releaseDate,
-        title: title ?? this.title,
-        voteAverage: voteAverage ?? this.voteAverage,
-        voteCount: voteCount ?? this.voteCount,
-      );
-
-  factory MovieModel.fromJson(Map<String, dynamic> json) => MovieModel(
-        backdropPath: json["backdrop_path"] ?? ' ',
-        genreIds: json["genre_ids"] == null
-            ? []
-            : List<int?>.from(json["genre_ids"]!.map((x) => x)),
+  factory MovieModel.fromMap(Map<String, dynamic> json) => MovieModel(
+        backdropPath: json["backdrop_path"] ?? '',
         id: json["id"],
-        originalTitle: json["original_title"],
         overview: json["overview"],
-        popularity: json["popularity"].toDouble(),
-        posterPath: json["poster_path"] ?? ' ',
-        releaseDate: DateTime.parse(json["release_date"]),
+        posterPath: json["poster_path"] ?? '',
         title: json["title"],
         voteAverage: json["vote_average"].toDouble(),
         voteCount: json["vote_count"],
